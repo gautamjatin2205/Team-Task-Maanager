b302c246961865df014fc00c293fe63152c26cc4
gautamjatin2205 <jatingautam2205@gmail.com>
2026-05-03 15:59:38 +0530

    update files

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
-import type { NextConfig } from "next";
-import path from "node:path";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  outputFileTracingRoot: path.join(__dirname)
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
